6ea7fba25421691d96d6564d557e9741ef6ef603
CongPhuc <phucb1506@gmail.com>
2018-02-04 22:45:59 +0700

    first commit

---
+++ app/src/main/java/congphuc/com/loginfacebook/MainActivity.java
@@ -0,0 +1,13 @@
+package congphuc.com.loginfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
